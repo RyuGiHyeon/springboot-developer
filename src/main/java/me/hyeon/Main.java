5981f312f46c579c77acb7ff8bb6fbece19cfe52
hyeon <ryuoo0123@gmail.com>
2024-01-14 19:22:12 +0900

    Exercies-08 완료 09장 test - TokenProvider.java -> generateToken() 문제

--- src/main/java/me/hyeon/Main.java
+++
@@ -1,7 +0,0 @@
-package me.hyeon;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- src/main/java/me/hyeon/springbootdeveloper/repository/BlogRepository.java
+++
@@ -1,8 +0,0 @@
-package me.hyeon.springbootdeveloper.repository;
-
-import me.hyeon.springbootdeveloper.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Article, Long> {
-
-}
