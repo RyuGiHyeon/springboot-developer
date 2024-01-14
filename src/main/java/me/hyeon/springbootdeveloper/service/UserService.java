fd2bdb9df36faef60380922b2dfba5f9f3393e78
hyeon <ryuoo0123@gmail.com>
2024-01-14 13:47:41 +0900

    Exercies-08 완료

---
+++ src/main/java/me/hyeon/springbootdeveloper/service/UserService.java
@@ -0,0 +1,24 @@
+package me.hyeon.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.hyeon.springbootdeveloper.domain.User;
+import me.hyeon.springbootdeveloper.dto.AddUserRequest;
+import me.hyeon.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto){
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                // 패스워드 암호화
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
